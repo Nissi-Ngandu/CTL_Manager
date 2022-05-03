08579ea5e4cd8073ec601eedbc25e077e7d0ae79
nissingandu <nissingandu@icloud>
2022-05-03 23:07:42 +0200

    Added new activities as well as made some UI changes.
    Handled page navigation between Login.java and Dashboard.java to test functionality.

--- app/src/main/java/com/legarz/ctl_manager/Login.java
+++ app/src/main/java/com/legarz/ctl_manager/Login.java
@@ -2,13 +2,31 @@ package com.legarz.ctl_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.legarz.ctl_manager.databinding.ActivityLoginBinding;
 
 public class Login extends AppCompatActivity {
+
+    /*Set activity binding for page*/
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        /*Handle login button click*/
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/legarz/ctl_manager/SignUp.java
@@ -0,0 +1,14 @@
+package com.legarz.ctl_manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
